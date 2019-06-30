df643f280bff5097866e2c5324bc195c3d70aebe
rfernandezv <richar.fernandezv@gmail.com>
2019-06-29 21:36:34 -0500

    Subir

--- build.sh
+++ build.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'development' ]]
-then
-  ng build --configuration=development
-fi
-
-if [[ $TRAVIS_BRANCH == 'qa' ]]
-then
-  ng build --configuration=qa
-fi
-
-if [[ $TRAVIS_BRANCH == 'uat' ]]
-then
-  ng build --configuration=uat
-fi
-
-if [[ $TRAVIS_BRANCH == 'prod' ]]
-then
-  ng build --prod
-fi
-
-if [[ $TRAVIS_BRANCH == 'master' ]]
-then
-  ng build --prod
-fi
+ng build --configuration=development
